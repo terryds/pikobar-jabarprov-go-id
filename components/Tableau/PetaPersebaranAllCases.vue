cd1090e53236edcf68b7f9a7d2efa441533e52cb
adrianpdm <adrian.padmodihardjo@gmail.com>
2020-03-16 21:12:50 +0700

    feat: tableau embedded frame

---
+++ components/Tableau/PetaPersebaranAllCases.vue
@@ -0,0 +1,17 @@
+<template>
+  <TableauViz :url="url"/>
+</template>
+
+<script>
+import TableauViz from './TableauViz'
+export default {
+  components: {
+    TableauViz
+  },
+  data () {
+    return {
+      url: 'https://public.tableau.com/views/PetaSebaranODPPDP-BlueVersion/DashboardMapAllCases?:embed=y&:origin=viz_share_link&:toolbar=n&:embed_code_version=3&:loadOrderID=0&showTabs=false&:display_count=y'
+    }
+  }
+}
+</script>
---
+++ components/Tableau/TableauViz.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="vizContainer" />
+</template>
+
+<script>
+export default {
+  props: {
+    url: {
+      type: String,
+      required: true
+    }
+  },
+  mounted () {
+    const placeholderDiv = document.getElementById('vizContainer')
+    const options = {
+      showTabs: false,
+      hideToolbar: true,
+      hideTabs: true
+    }
+    // eslint-disable-next-line
+    this.viz = new tableau.Viz(placeholderDiv, this.url, options)
+  }
+}
+</script>
+
+<style>
+
+</style>
