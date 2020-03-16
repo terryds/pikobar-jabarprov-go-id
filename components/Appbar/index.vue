3f000e1c7c2bb2932f09d09da4da21b0e706a8af
adrianpdm <adrian.padmodihardjo@gmail.com>
2020-03-16 16:17:30 +0700

    chore: add back button iin appbar

--- components/Appbar/index.vue
+++ components/Appbar/index.vue
@@ -1,7 +1,22 @@
 <template>
   <header class="bg-white border-b border-solid border-gray-300">
     <div class="container mx-auto p-4">
-      <div class="flex items-center">
+      <a
+        v-if="canGoBack"
+        tag="a"
+        class="cursor-pointer flex items-center"
+        href="javascript:void(0)"
+        @click.prevent="onGoBack">
+        <FontAwesomeIcon :icon="icon.faChevronLeft" />
+        <div class="text-left px-4">
+          <p class="text-lg">
+            Kembali
+          </p>
+        </div>
+      </a>
+      <div
+        v-else
+        class="flex items-center">
         <nuxt-link to="/">
           <img class="block h-10 mx-auto mr-4" src="logo.jpg" alt>
         </nuxt-link>
@@ -21,8 +36,28 @@
 </template>
 
 <script>
+import { faChevronLeft } from '@fortawesome/free-solid-svg-icons'
 export default {
-
+  data () {
+    return {
+      icon: {
+        faChevronLeft
+      }
+    }
+  },
+  computed: {
+    canGoBack () {
+      return this.$route.path !== '/'
+    },
+    pathToGoBack () {
+      return this.$store.state.route.from.path
+    }
+  },
+  methods: {
+    onGoBack () {
+      this.$router.back()
+    }
+  }
 }
 </script>
 
