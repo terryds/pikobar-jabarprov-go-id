86c095dee7e07cf1e85e37a78c8f2bed8ed4b5c7
adrianpdm <adrian.padmodihardjo@gmail.com>
2020-03-17 00:59:10 +0700

    minor: ripple effect to indicate clickable button

--- components/CallCard/index.vue
+++ components/CallCard/index.vue
@@ -1,10 +1,10 @@
 <template>
   <div
-    class="cursor-pointer px-3 py-5 flex flex-row justify-start items-center bg-green-100 hover:bg-green-400"
+    class="call-card cursor-pointer px-3 py-5 flex flex-row justify-start items-center bg-green-100 hover:bg-green-200"
     style="border-radius: 0.8rem; box-shadow: 0 0 4px 0px rgba(0,0,0,0.05), 0 4px 24px 0 rgba(0,0,0,0.1);"
     @click.capture="onClick">
     <FontAwesomeIcon
-      class="mr-3 text-xl text-green-800 pointer-events-none"
+      class="mr-3 text-xl text-green-900 pointer-events-none"
       :icon="icon.faPhone"/>
     <div class="pointer-events-none text-green-700">
       <label class="block text-base" v-text="title" />
@@ -48,6 +48,39 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
+@keyframes ripple {
+  0%, 35% {
+    transform: scale(0);
+    opacity: 0.5;
+  }
+  50% {
+    transform: scale(1.5);
+    opacity: 0.3;
+  }
+  100% {
+    opacity: 0;
+    transform: scale(4);
+  }
+}
 
+.call-card {
+  position: relative;
+  overflow: hidden;
+
+  &::after {
+    content: '';
+    mix-blend-mode: screen;
+    position: absolute;
+    width: 4rem;
+    height: 4rem;
+    left: -1rem;
+    background: rgba(51, 204, 84, 0.5);
+    opacity: 0;
+    border-radius: 100%;
+    transform-origin: 50% 50%;
+    animation: ripple 1.2s ease-out infinite;
+    animation-delay: 1s;
+  }
+}
 </style>
