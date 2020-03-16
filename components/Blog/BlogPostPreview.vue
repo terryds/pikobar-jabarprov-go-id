044c74c659f836c5ad5ee0088a50becc955daba9
adrianpdm <adrian.padmodihardjo@gmail.com>
2020-03-17 01:55:34 +0700

    feat: show cover image on larger viewport

--- components/Blog/BlogPostPreview.vue
+++ components/Blog/BlogPostPreview.vue
@@ -1,30 +1,36 @@
 <template>
   <div>
-    <header>
-      <nuxt-link
-        tag="a"
-        :to="to"
-        class="text-lg font-bold hover:underline">
-        {{ title }}
-      </nuxt-link>
-      <p class="text-base">
-        {{ subtitle }}
-      </p>
-    </header>
-    <p class="opacity-75">
-      {{ date }}
-    </p>
-    <br>
-    <section>
-      <!-- eslint-disable-next-line -->
-      <div class="content" v-html="truncatedContent" />
-    </section>
+    <div class="md:flex flex-row justify-start items-start">
+      <section class="md:w-2/3 flex-shrink flex-grow-0 mr-6">
+        <header>
+          <nuxt-link
+            tag="a"
+            :to="to"
+            class="text-lg font-bold hover:underline">
+            {{ title }}
+          </nuxt-link>
+          <p class="text-base">
+            {{ subtitle }}
+          </p>
+        </header>
+        <p class="opacity-75">
+          {{ date }}
+        </p>
+        <br>
+        <!-- eslint-disable-next-line -->
+        <div class="content flex-1" v-html="truncatedContent" />
+      </section>
+      <img
+        v-if="image"
+        class="hidden md:block w-1/3 self-stretch rounded-lg overflow-hidden object-cover object-center flex-none"
+        :src="image">
+    </div>
     <div class="clearfix my-2">
       <nuxt-link
-        class="float-right text-brand-blue hover:text-brand-blue-lighter font-bold"
+        class="float-right md:float-left md:mt-4 text-brand-blue hover:text-brand-blue-lighter font-bold"
         :to="to">
         <span class="mr-2 align-middle uppercase">
-          Baca
+          Selengkapnya
         </span>
         <FontAwesomeIcon class="align-middle" :icon="icon.faChevronRight" />
       </nuxt-link>
@@ -36,6 +42,10 @@
 import { faChevronRight } from '@fortawesome/free-solid-svg-icons'
 export default {
   props: {
+    image: {
+      type: String,
+      default: null
+    },
     title: {
       type: String,
       default: ''
