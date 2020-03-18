98365f376f41dc9b9e22bae428718672ce894e48
maruf12 <maruf.harsono@gmail.com>
2020-03-18 09:13:54 +0700

    redesign page hospital

--- pages/hospitals/index.vue
+++ pages/hospitals/index.vue
@@ -1,12 +1,21 @@
 <template>
   <div class="container sm:p-5 mx-auto">
-    <div class="p-5 bg-white border-solid border-gray-300">
+    <div class="py-20 bg-auto bg-green-400">
+      <h2 class="text-2xl font-bold leading-tight">
+        Kontak Rumah Sakit dan Call Center
+      </h2>
+      <p class="mb-6 mt-2 text-base opacity-50 leading-tight">
+        Informasi dan nomor alamat rumah sakit yang menjadi rujukan pemeriksaan gejala COVID-19
+      </p>
+    </div>
+
+    <div class="p-5 bg-white border-solid border border-gray-400 rounded-lg">
       <section>
-        <h2 class="text-xl font-bold leading-tight">
+        <h2 class="text-2xl font-bold leading-tight">
           Daftar Rumah Sakit Rujukan
         </h2>
-        <p class="mb-8 text-lg opacity-50 leading-tight">
-          di Jawa Barat
+        <p class="mb-6 mt-2 text-base opacity-50 leading-tight">
+          Berikut ini adalah rumah sakit yang menjadi rujukan untuk pasien dengan status Pasien dalam Pengawasan. Anda harus mengunjungi fasilitas kesehatan terdekat terlebih dahulu seperti klinik/rumah sakit umum sebelum akhirnya dapat dirujuk ke rumah sakit di bawah ini.
         </p>
         <br>
         <div>
@@ -14,15 +23,23 @@
         </div>
       </section>
     </div>
+    <div class="mb-6 mt-8 p-5 bg-white border-solid border border-gray-400 rounded-lg">
+      <h2 class="mb-6 text-2xl font-bold leading-tight">
+        Hubungi Call Center
+      </h2>
+      <CallCenter />
+    </div>
   </div>
 </template>
 
 <script>
 import { mapState } from 'vuex'
 import ContactList from '~/components/ContactList'
+import CallCenter from '~/components/CallCenter'
 export default {
   components: {
-    ContactList
+    ContactList,
+    CallCenter
   },
   computed: {
     ...mapState('hospitals', {
