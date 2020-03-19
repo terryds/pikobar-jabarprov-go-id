cc972c92057125f8ef657ac11c20553b3ff1af79
adrianpdm <adrian.padmodihardjo@gmail.com>
2020-03-20 00:51:05 +0700

    minor: container, navbar, and appbar css

--- components/Appbar/index.vue
+++ components/Appbar/index.vue
@@ -1,7 +1,10 @@
 <template>
-  <header class="bg-white border-b border-solid border-gray-300">
+  <header
+    class="sticky top-0 z-10 bg-white border-b border-solid border-gray-300"
+    style="box-shadow: 0 0px 32px -4px rgba(0,0,0,0.15)"
+  >
     <div v-if="showPopupNotification" class="bg-brand-yellow-darkest">
-      <div class="container mx-auto">
+      <div class="container mx-auto ">
         <div class="flex flex-wrap px-6 py-4">
           <div class="text-sm w-full">
             Izinkan notifikasi mengirim pesan
@@ -13,52 +16,119 @@
       </div>
     </div>
     <div class="container mx-auto p-4">
-      <a
-        v-if="canGoBack"
-        tag="a"
-        class="cursor-pointer flex items-center"
-        href="javascript:void(0)"
-        @click.prevent="onGoBack"
-      >
-        <FontAwesomeIcon :icon="icon.faChevronLeft" />
-        <div class="text-left px-4">
-          <p class="text-lg">
-            Kembali
-          </p>
-        </div>
-      </a>
-      <div
-        v-else
-        class="flex items-center"
-      >
-        <nuxt-link to="/">
-          <img class="block h-10 mx-auto mr-4" src="logo.jpg" alt>
-        </nuxt-link>
-        <nuxt-link to="/">
-          <div class="text-left">
-            <p class="text-base">
-              Pusat Informasi &amp; Koordinasi COVID-19
-            </p>
-            <p class="text-sm text-gray-700">
-              Provinsi Jawa Barat
+      <div class="md:hidden">
+        <a
+          v-if="canGoBack"
+          tag="a"
+          class="cursor-pointer flex items-center"
+          href="javascript:void(0)"
+          @click.prevent="onGoBack"
+        >
+          <FontAwesomeIcon :icon="icon.faChevronLeft" />
+          <div class="text-left px-4">
+            <p class="text-lg">
+              Kembali
             </p>
           </div>
-        </nuxt-link>
+        </a>
+        <div v-else class="flex justify-between items-center">
+          <div
+            class="flex items-center"
+          >
+            <nuxt-link to="/">
+              <img class="block h-10 mx-auto mr-4" src="logo.jpg" alt>
+            </nuxt-link>
+            <nuxt-link to="/">
+              <div class="text-left">
+                <p class="text-base">
+                  Pusat Informasi &amp; Koordinasi COVID-19
+                </p>
+                <p class="text-sm text-gray-700">
+                  Provinsi Jawa Barat
+                </p>
+              </div>
+            </nuxt-link>
+          </div>
+        </div>
+      </div>
+      <div class="hidden md:block md:mx-4">
+        <div class="flex justify-between items-center">
+          <div
+            class="flex items-center"
+          >
+            <nuxt-link to="/">
+              <img class="block h-10 mx-auto mr-4" src="logo.jpg" alt>
+            </nuxt-link>
+            <nuxt-link to="/">
+              <div class="text-left">
+                <p class="text-base">
+                  Pusat Informasi <br class="inline-block lg:hidden"> &amp;  Koordinasi COVID-19
+                </p>
+                <p class="text-sm text-gray-700">
+                  Provinsi Jawa Barat
+                </p>
+              </div>
+            </nuxt-link>
+          </div>
+          <ul class="flex flex-row items-center">
+            <nuxt-link
+              v-for="item in menus"
+              :key="item.to"
+              tag="li"
+              :to="item.to"
+              :exact="item.exact"
+              class="cursor-pointer mx-2 px-2 py-2 hover:bg-gray-200"
+            >
+              <a>
+                {{ item.label }}
+              </a>
+            </nuxt-link>
+            <nuxt-link
+              tag="li"
+              to="/notifications"
+              :exact="true"
+              class="cursor-pointer w-12 h-12 flex items-center justify-center rounded-full hover:bg-gray-200 box-content"
+            >
+              <a class="text-gray-500">
+                <FontAwesomeIcon
+                  :icon="icon.faBell"
+                />
+              </a>
+            </nuxt-link>
+            <nuxt-link
+              v-if="enableDownload"
+              tag="li"
+              :to="downloadAppURL"
+              class="cursor-pointer px-4 py-2 text-white text-center ml-2 rounded-md bg-brand-green hover:bg-brand-green-light"
+            >
+              Download App
+            </nuxt-link>
+          </ul>
+        </div>
       </div>
     </div>
   </header>
 </template>
 
 <script>
-import { faChevronLeft } from '@fortawesome/free-solid-svg-icons'
+import { faChevronLeft, faBell } from '@fortawesome/free-solid-svg-icons'
 import { db, messaging, FieldValue } from '~/lib/firebase'
 export default {
   data () {
     return {
       icon: {
-        faChevronLeft
+        faChevronLeft,
+        faBell
       },
-      showPopupNotification: false
+      showPopupNotification: false,
+      menus: [
+        { to: '/', label: 'Home', exact: true },
+        { to: '/articles?tab=jabar', label: 'Berita' },
+        { to: '/contact', label: 'Kontak' },
+        { to: '/faq', label: 'FAQ' }
+      ],
+      enableDownload: false,
+      downloadAppURL: '#'
     }
   },
   computed: {
@@ -70,11 +140,30 @@ export default {
     }
   },
   mounted () {
+    this.toggleDownloadAppButton()
     this.checkPermission()
   },
   methods: {
     onGoBack () {
       this.$router.back()
+    },
+    toggleDownloadAppButton () {
+      if (process.client || process.browser) {
+        import('../../lib/firebase-client')
+          .then((m) => {
+            return m ? m.default || m : null
+          }).then((module) => {
+            if (!module) {
+              return
+            }
+            const { remoteConfig } = module
+            remoteConfig.fetchAndActivate()
+              .then(() => {
+                this.enableDownload = remoteConfig.getValue('download_app_via_web_enabled') === 'true'
+                this.downloadAppURL = remoteConfig.getValue('download_app_url')._value
+              })
+          })
+      }
     },
     async checkPermission () {
       if (!messaging) {
