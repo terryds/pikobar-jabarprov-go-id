219e3bb8596b1f9848bbe25c3f8b815b5e919abe
adrianpdm <adrian.padmodihardjo@gmail.com>
2020-03-23 16:02:44 +0700

    chore: move remote-config data consumption to its own store

--- plugins/initial-data.js
+++ plugins/initial-data.js
@@ -1,27 +1,6 @@
-import { remoteConfig } from '~/lib/firebase-client'
-
 export default async function ({ app }) {
-  let retry = 0
-  const fetch = () => {
-    if (retry >= 5) {
-      return
-    }
-    remoteConfig.fetchAndActivate()
-      .then(() => {
-        const selfDiagnoseURL = remoteConfig.getValue('selfdiagnose_url')._value
-        app.store.commit('corona/setSelfDiagnoreURL', selfDiagnoseURL)
-      }).catch((e) => {
-        retry += 1
-        fetch()
-      })
-  }
-  remoteConfig.activate()
-    .then((isActivated) => {
-      if (!isActivated) {
-        fetch()
-      }
-    })
   await Promise.all([
+    app.store.dispatch('remote-config/getConfig'),
     app.store.dispatch('statistics/getCases'),
     app.store.dispatch('hospitals/getItems')
   ])
