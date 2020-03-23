3a20be1e9d6948c77a867c9dfec1d508ffd68d3c
adrianpdm <adrian.padmodihardjo@gmail.com>
2020-03-23 14:15:20 +0700

    fix: relayout statistics widget

--- components/_pages/index/StatisticLoader.vue
+++ components/_pages/index/StatisticLoader.vue
@@ -22,9 +22,9 @@ export default {
       class: ['bg-white rounded-lg mb-8 lg:mb-0 lg:w-1/2 shadow-md', staticClass],
       style: staticStyle
     }
-    if (context.props.isPending) {
-      return (
-        <div {...containerProps} class="p-5">
+    return (
+      <div {...containerProps}>
+        <div style={`display: ${context.props.isPending ? 'block' : 'none'}`} class="p-5">
           <ContentLoader
             speed={2}
             width={400}
@@ -35,65 +35,63 @@ export default {
             <rect x={0} y={0} rx={8} ry={6} width="50%" height="16"></rect>
             <rect x={0} y={30} rx={8} ry={6} width="66%" height="16"></rect>
             <rect x={0} y={64} rx={8} ry={6} width="20%" height="16"></rect>
-          </ContentLoader>
-        </div>
-      )
-    }
-    return (
-      <div {...containerProps}>
-        <h3 class="p-5" style="font-size: 23px !important;">
-          <b>
-            { context.props.label }
-          </b>
-        </h3>
-        <hr />
-        <div class="p-5 flex flex-col md:flex-row md:justify-start md:items-end leading-none">
-          <div class="mr-8 mb-4 md:mb-0">
-            <span
-              class="text-4xl font-bold inline-block"
-              style="color: #2DAC55; min-width: 2ch;">
-              { context.props.processed }
-            </span>
-            <span
-              class="text-sm font-bold ml-2"
-              style="color: #000000;">
-              { `(${context.props.processedPercentage}% )` }
-            </span>
-            <p
-              class="mt-2 text-base whitespace-no-wrap"
-              style="color: #8A8A8A;">
-              { context.props.processedLabel }
-            </p>
+          </ContentLoader></div>
+        <div style={`display: ${context.props.isPending ? 'none' : 'block'}`}>
+          <h3 class="p-5" style="font-size: 23px !important;">
+            <b>
+              { context.props.label }
+            </b>
+          </h3>
+          <hr />
+          <div class="p-5 flex flex-col md:flex-row md:justify-start md:items-end leading-none">
+            <div class="mr-8 mb-4 md:mb-0">
+              <span
+                class="text-4xl font-bold inline-block"
+                style="color: #2DAC55; min-width: 2ch;">
+                { context.props.processed }
+              </span>
+              <span
+                class="text-sm font-bold ml-2"
+                style="color: #000000;">
+                { `(${context.props.processedPercentage}% )` }
+              </span>
+              <p
+                class="mt-2 text-base whitespace-no-wrap"
+                style="color: #8A8A8A;">
+                { context.props.processedLabel }
+              </p>
+            </div>
+            <div class="mr-8 mb-4 md:mb-0">
+              <span
+                class="text-2xl font-bold inline-block"
+                style="color: #2DAC55; min-width: 2ch;">
+                { context.props.finished }
+              </span>
+              <span
+                class="text-sm font-bold ml-2"
+                style="color: #000000;">
+                { `(${context.props.finishedPercentage}%)` }
+              </span>
+              <p
+                class="mt-2 text-base whitespace-no-wrap "
+                style="color: #8A8A8A;">
+                { context.props.finishedLabel }
+              </p>
+            </div>
+            <div class="mr-8 mb-4 md:mb-0">
+              <span
+                class="text-2xl font-bold inline-block"
+                style="color: #2DAC55; min-width: 2ch;">
+                { context.props.total }
+              </span>
+              <p
+                class="mt-2 text-base whitespace-no-wrap"
+                style="color: #8A8A8A;">
+                { context.props.totalLabel }
+              </p>
+            </div>
           </div>
-          <div class="mr-8 mb-4 md:mb-0">
-            <span
-              class="text-2xl font-bold inline-block"
-              style="color: #2DAC55; min-width: 2ch;">
-              { context.props.finished }
-            </span>
-            <span
-              class="text-sm font-bold ml-2"
-              style="color: #000000;">
-              { `(${context.props.finishedPercentage}%)` }
-            </span>
-            <p
-              class="mt-2 text-base whitespace-no-wrap "
-              style="color: #8A8A8A;">
-              { context.props.finishedLabel }
-            </p>
-          </div>
-          <div class="mr-8 mb-4 md:mb-0">
-            <span
-              class="text-2xl font-bold inline-block"
-              style="color: #2DAC55; min-width: 2ch;">
-              { context.props.total }
-            </span>
-            <p
-              class="mt-2 text-base whitespace-no-wrap"
-              style="color: #8A8A8A;">
-              { context.props.totalLabel }
-            </p>
-          </div>
+          { context.slots().footnote }
         </div>
       </div>
     )
