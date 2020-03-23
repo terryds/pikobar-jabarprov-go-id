aa616fecb440e7fe5c36e55fbe481fe2b4d9d409
adrianpdm <adrian.padmodihardjo@gmail.com>
2020-03-23 14:12:56 +0700

    fix: move data source to firebase

--- api/statistics.js
+++ api/statistics.js
@@ -8,6 +8,18 @@ export function getCases (options) {
       if (doc.exists) {
         const data = doc.data()
         data.updated_at = data.updated_at.toDate()
+        Object.assign(data.odp, {
+          proses: {
+            jabar: (data.odp.total.jabar || 0) - (data.odp.selesai.jabar || 0),
+            nasional: (data.odp.total.nasional || 0) - (data.odp.selesai.nasional || 0)
+          }
+        })
+        Object.assign(data.pdp, {
+          proses: {
+            jabar: (data.pdp.total.jabar || 0) - (data.pdp.selesai.jabar || 0),
+            nasional: (data.pdp.total.nasional || 0) - (data.pdp.selesai.nasional || 0)
+          }
+        })
         return {
           ...data
         }
