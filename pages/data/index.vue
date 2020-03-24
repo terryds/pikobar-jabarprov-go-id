3480dd20359f3a0b3643e2114d28da3033a98b51
adrianpdm <adrian.padmodihardjo@gmail.com>
2020-03-24 17:04:20 +0700

    trigger deploy

--- pages/data/index.vue
+++ pages/data/index.vue
@@ -21,7 +21,7 @@
 
       <section class="row">
         <div
-          class="bg-white col-md-12 p-0 m-2 "
+          class="bg-white col-md-12 p-0 m-2"
           style="border-radius: 0.8rem; box-shadow: 0 0 4px 0px rgba(0,0,0,0.05), 0 4px 24px 0 rgba(0,0,0,0.1); height:50em;"
         >
           <MapView />
